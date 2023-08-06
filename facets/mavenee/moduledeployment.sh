df19db9dd44d0b2f997eb33d24df38b2221fc1a2
masumcse1 <optinfra.masum@gmail.com>
2023-08-07 00:46:22 +0600

    correction.

--- facets/mavenee/moduledeployment.sh
+++ facets/mavenee/moduledeployment.sh
@@ -5,7 +5,7 @@
 USERNAME="masum"
 PASSWORD="Masum!2345"
 wildfy_home="/opt/alldata/wildfly-18.0.0.Final"
-deploymnet_folder="$wildfy_home/standalone/deployments"
+deployment_folder="$wildfy_home/standalone/deployments"
 temp_folder="$wildfy_home/standalone/databackup"
 temp_dir="$wildfy_home/standalone/databackup/temp"
 application_xml="META-INF/application.xml"
@@ -18,7 +18,7 @@ module_war_file="modulecode.war"
 ##-----------copy meveo.ear file into a temporary place-------------------------------------
 
 mkdir -p "$temp_folder"
-cp "$deploymnet_folder/$ear_file" "$temp_folder"
+cp "$deployment_folder/$ear_file" "$temp_folder"
 
 ##-----------copy meveo.ear file into a temporary place-------------------------------------
 unzip $temp_folder/$ear_file -d $temp_dir  >/dev/null 2>&1
@@ -34,13 +34,13 @@ cd ..
 rm -rf $temp_dir
 echo "File copied inside the EAR file: $new_ear_file"
 #----------------module war file & -module config  copy into a meveo.ear -----------------------
-rm "$deploymnet_folder/$ear_file"
+rm "$deployment_folder/$ear_file"
 echo "File $file_name removed successfully."
    
 #-------------------remveo meveo.ear file ---------------------
 /$wildfy_home/bin/jboss-cli.sh --connect --command="reload"
 #----------------------reload wildfy -----------------------------------
-cp "$temp_folder/$ear_file" "$deploymnet_folder"
+cp "$temp_folder/$ear_file" "$deployment_folder"
 #------------deploymnet new meveo.ear file with module war file
 rm -r $temp_folder
 #remove databackup dir 
