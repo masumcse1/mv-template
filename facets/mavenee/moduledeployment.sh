63edd9d0c7a9d8cd04e3f121f0e76c3289d82ffe
masumcse1 <optinfra.masum@gmail.com>
2023-08-20 01:41:28 +0600

    java.lang.OutOfMemoryError: Metaspace solved .

--- facets/mavenee/moduledeployment.sh
+++ facets/mavenee/moduledeployment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wildfy_home="wildfypath"
+wildfy_home="/opt/jboss/wildfy"
 deployment_folder="$wildfy_home/standalone/deployments"
 temp_folder="$wildfy_home/standalone/databackup"
 ear_file="meveo.ear"
@@ -8,7 +8,7 @@ ear_file="meveo.ear"
 #-------------------remveo meveo.ear file -----------------------------
 rm "$deployment_folder/$ear_file*"
 #----------------------reload wildfy -----------------------------------
-/$wildfy_home/bin/jboss-cli.sh --connect --command="reload"
+/$wildfy_home/bin/jboss-cli.sh --connect --command="shutdown --restart=true"
 #---------------------copy meveo.ear filec #-deploymnet new meveo.ear---
 cp "$temp_folder/$ear_file" "$deployment_folder"
 #------------remove temporary file--------------------------------------
